f0c4e18f09b4076c4e96ea72501b188c362203de
Nalipp <nateplipp@gmail.com>
2017-10-18 22:20:04 -0500

    fix guessing game

--- javascript_advanced_topics/closures/exercises/guessingGame.js
+++ javascript_advanced_topics/closures/exercises/guessingGame.js
@@ -15,48 +15,23 @@ If the guess is too high return "Your guess is too high!"
 You should stop the user from guessing if the amount of guesses they have made is greater than the initial amount passed to the outer function.
 
 You will have to make use of closure to solve this problem.
-
-Examples (yours might not be like this, since the answer is random every time):
-
-    var game = guessingGame(5)
-    game(1) // "You're too low!"
-    game(8) // "You're too high!"
-    game(5) // "You're too low!"
-    game(7) // "You got it!"
-    game(1) // "You are all done playing!"
-
-    var game2 = guessingGame(3)
-    game2(5) // "You're too low!"
-    game2(3) // "You're too low!"
-    game2(1) // "No more guesses the answer was 0"
-    game2(1) // "You are all done playing!"
 */
 
 function guessingGame(amount){
   var answer = Math.floor(Math.random() * 11);
   var guesses = 0;
-  console.log(answer);
-  return (guess) => {
-    if (guesses >= amount) return "You are all done playing!";
-    if (guess === answer) {
+  return function (guess) {
+    if (guesses >= amount) {
+      return "All done playing!";
+    guesses++;
+    if(guess === answer) {
       guesses = Number.MAX_VALUE;
-      return "You got it!"; 
+      return "You got it!";
     }
-    if (guess > answer) {
-      guesses += 1;
-      return "Your guess is too high!";
+    if (guesses === amount) {
+      return "No more guesses the answer was " + answer;
     }
-    if (guess < answer) {
-      guesses += 1;
-      return "Your guess is too low!";
-    }
-  };
+    if (guess > answer) return "Your guess is too high!";
+    if (guess < answer) return "Your guess is too low!";
+  }
 }
-
-var game1 = guessingGame(3);
-
-console.log(game1(1));
-console.log(game1(3));
-console.log(game1(5));
-console.log(game1(9));
-console.log(game1(8));

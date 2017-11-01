e1711b63e37f0cc23b258238a46a9a66f7be28e2
Nalipp <nateplipp@gmail.com>
2017-10-31 21:53:55 -0500

    coursework and exercises

--- es2015_2016/practice.js
+++ es2015_2016/practice.js
@@ -14,11 +14,37 @@
 // hoist() // undefined
 
 
-for (var i = 0; i < 5; i++) {
-  (function(j) {
-    setTimeout(function() {
-      console.log(j);
-    },0 )
-  })(i);
-}
+// for (var i = 0; i < 5; i++) {
+//   (function(j) {
+//     setTimeout(function() {
+//       console.log(j);
+//     },0 )
+//   })(i);
+// }
 
+
+// let greeting = {
+//   name : 'nate',
+//   greet : function() {
+//     setTimeout(() => {
+//       console.log(`Hi there ${this.name}`);
+//     }, 1000);
+//   }
+// }
+
+// console.log(greeting.name);
+// greeting.greet();
+
+// bind is used insead of call or apply because we do not want to immediatley invoke the function
+
+// let talk = (word1, word2) => {
+//   console.log(arguments);
+// }
+
+// talk('hi', 'bye');
+
+// function talk(word1, word2) {
+//   console.log(arguments);
+// }
+
+// talk('hi', 'bye');

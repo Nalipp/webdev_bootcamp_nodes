01f3f63cd570af2b2a04a655c5d99dccf483fd7f
Nalipp <nateplipp@gmail.com>
2017-12-13 17:17:32 -0600

    map card colors

--- react/exercises/memory-game/src/CardList.js
+++ react/exercises/memory-game/src/CardList.js
@@ -3,30 +3,44 @@ import './CardList.css';
 import PropTypes from 'prop-types';
 import Card from './Card';
 
+const COLORS = ['crimson', 'lawngreen', 'dodgerblue', 'pink', 'violet', 'orange', 'yellow'];
 
 class CardList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      clickedColor: null,
+    }
+  }
+
   render() {
     const cards = this.props.allCards.map((c, i) => (
       <Card key={i} color={c.color}/>
     ));
     return (
-      <div className="CardList">
+      <div className="cardList">
         {cards}
       </div>
     );
   }
 }
 
+function mapColors(arr) {
+  let arrCopy1 = arr.slice();
+  let arrCopy2 = arr.slice();
+  let returnArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let rand1 = Math.floor(Math.random() * arrCopy1.length);
+    let rand2 = Math.floor(Math.random() * arrCopy2.length);
+    returnArr.push({color: arrCopy1.splice(rand1, 1)});
+    returnArr.push({color: arrCopy2.splice(rand2, 1)});
+  }
+  return returnArr;
+}
+
 CardList.defaultProps = {
-  allCards: [
-    {color: 'crimson'}, 
-    {color: 'lawngreen'}, 
-    {color: 'dodgerblue'}, 
-    {color: 'pink'}, 
-    {color: 'violet'}, 
-    {color: 'orange'}, 
-    {color: 'yellow'}, 
-  ]
+  allCards: mapColors(COLORS)
 }
 
 CardList.propTypes = {

cf5f6c7bea615d95a7a8e1c92af1a356d9a60d46
Nalipp <nateplipp@gmail.com>
2017-12-22 10:13:15 -0600

    delete api request

--- react/projects/full_stack_todo_app/todosfrontend/src/TodoList.js
+++ react/projects/full_stack_todo_app/todosfrontend/src/TodoList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 import TodoForm from './TodoForm';
-const APIURL = '/api/todos';
+const APIURL = '/api/todos/';
 
 class TodoList extends Component {
   constructor(props) {
@@ -55,17 +55,39 @@ class TodoList extends Component {
       return res.json()
     }).then(todos => this.setState({todos}));
   }
+  deleteTodo(id) {
+    fetch(APIURL + id, {
+      method: 'delete',
+    })
+    .then(res => {
+      if(!res.ok) {
+        if(res.status >= 400 && res.status <= 500) {
+          return res.json().then(data => {
+            let err = {errorMessage: data.message};
+            throw err;
+          })
+        } else {
+          let err = {errorMessage: 'Server is not responding'};
+          throw err;
+        }
+      }
+    })
+    .then(() => {
+      let todos = this.state.todos.filter(todo => todo._id !== id);
+      this.setState({todos});
+    })
+  }
   render() {
     const todo = this.state.todos.map((todo) => (
       <TodoItem 
         key={todo._id} 
-        name={todo.name} 
-        completed={todo.completed} />
+        {...todo}
+        onDelete={this.deleteTodo.bind(this, todo._id)} />
     ))
     return (
       <div>
         <h1>Todo List</h1>
-        <TodoForm addTodo={this.addTodo}/>
+        <TodoForm addTodo={this.addTodo} />
         <ul>
           {todo}
         </ul>
